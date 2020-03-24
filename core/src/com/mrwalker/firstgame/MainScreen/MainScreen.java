d086dd484960b326bf99281e53ace3489e38df72
szymon <szymon.janas.95@gmail.com>
2020-03-24 12:49:06 +0100

    #1 v0.0.1 Sceleton - Main Screen

---
+++ core/src/com/mrwalker/firstgame/MainScreen/MainScreen.java
@@ -0,0 +1,61 @@
+package com.mrwalker.firstgame.MainScreen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mrwalker.firstgame.GameMain;
+
+public class MainScreen  implements Screen {
+
+    private GameMain gameMain;
+
+    SpriteBatch batch;
+    Texture img;
+
+    public MainScreen(GameMain gameMain) {
+        this.gameMain = gameMain;
+    }
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+        img = new Texture("player.jpg");
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+}
