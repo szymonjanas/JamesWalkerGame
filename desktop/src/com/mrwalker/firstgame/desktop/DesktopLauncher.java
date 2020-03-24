ad5a2e992a57399577f66d4fb7fe4ca66e95f9f8
szymon <szymon.janas.95@gmail.com>
2020-03-24 12:31:37 +0100

    Initial commit

---
+++ desktop/src/com/mrwalker/firstgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mrwalker.firstgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mrwalker.firstgame.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
