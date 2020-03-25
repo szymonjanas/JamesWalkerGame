472b01abc3e9c87f4ba40184dd8c2c246c9c919b
szymon <szymon.janas.95@gmail.com>
2020-03-25 01:28:22 +0100

    Added classes to support size and position
    instead of Vector2, which has big problem with encapsulation.

---
+++ core/src/com/mrwalker/firstgame/auxiliary/Size2.java
@@ -0,0 +1,28 @@
+package com.mrwalker.firstgame.auxiliary;
+
+public class Size2 {
+        private float width;
+        private float height;
+
+    public Size2(float width, float height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+}
