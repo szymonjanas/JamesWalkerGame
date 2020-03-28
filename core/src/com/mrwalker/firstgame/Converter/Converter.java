4d9a3d09426b77b62e0a7e54442a5f12fdf65b0b
szymon <szymon.janas.95@gmail.com>
2020-03-28 23:22:41 +0100

    Refactor: Config class added, converter get info from config

--- core/src/com/mrwalker/firstgame/Converter/Converter.java
+++ core/src/com/mrwalker/firstgame/Converter/Converter.java
@@ -1,20 +1,22 @@
 package com.mrwalker.firstgame.Converter;
 
 import com.badlogic.gdx.math.Vector2;
+import com.mrwalker.firstgame.Config.Config;
 import com.mrwalker.firstgame.auxiliary.Position2;
 
 public class Converter {
+
+    private static final int tileWidth = Config.tileWidth, tileHeight = Config.tileHeight;
+
     public static Position2 cartesianToIsometric(Position2 position){
-        float tileWidth = 64, tileHeight = 32;
         Vector2 point = new Vector2(position.getX(), position.getY());
         point.x /= tileWidth;
-        point.y = (point.y - tileHeight / 2) / tileHeight + point.x;
+        point.y = (point.y - tileHeight / 2f) / tileHeight + point.x;
         point.x -= point.y - point.x;
-        return new Position2(point.x * tileWidth/2, point.y * tileHeight);
+        return new Position2(point.x * tileWidth / 2f, point.y * tileHeight);
     }
 
     public static Position2 isometricToCartesian(Position2 position){
-        float tileWidth = 64, tileHeight = 32;
         Vector2 point = new Vector2(position.getX(), position.getY());
         point.x = position.getY() + position.getX();
         point.y = (position.getY() - position.getX()) / 2f + 16;
