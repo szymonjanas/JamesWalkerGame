a5cde38a72e7e076c76331e6ae74a94af77fd2e2
szymon <szymon.janas.95@gmail.com>
2020-04-04 20:54:50 +0200

    Add: Addictional options to Position2

--- core/src/com/mrwalker/firstgame/auxiliary/Position2.java
+++ core/src/com/mrwalker/firstgame/auxiliary/Position2.java
@@ -1,5 +1,7 @@
 package com.mrwalker.firstgame.auxiliary;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class Position2 {
     private float x;
     private float y;
@@ -7,6 +9,11 @@ public class Position2 {
     public Position2(float x, float y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Position2(Vector2 position){
+        this.x = position.x;
+        this.y = position.y;
     }
 
     public Position2(){}
@@ -29,5 +36,9 @@ public class Position2 {
 
     public String toString(){
         return "Position x: " + x + ", y: " + y;
+    }
+
+    public Vector2 toVector2(){
+        return new Vector2(this.x, this.y);
     }
 }
