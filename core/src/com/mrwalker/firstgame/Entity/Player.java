12e927cbb5fdfd2290d563cafda7558abd0c070d
szymon <szymon.janas.95@gmail.com>
2020-04-05 01:30:39 +0200

    Refactor: move player to entity

--- core/src/com/mrwalker/firstgame/Entity/Player.java
+++ core/src/com/mrwalker/firstgame/Entity/Player.java
@@ -1,10 +1,9 @@
-package com.mrwalker.firstgame;
+package com.mrwalker.firstgame.Entity;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
-import com.mrwalker.firstgame.Entity.Entity;
 import com.mrwalker.firstgame.Utility.EntityUtility;
 import com.mrwalker.firstgame.auxiliary.Position2;
 
@@ -37,29 +36,33 @@ public class Player extends Entity {
 
     public void setPosition(Position2 position){
         this.position = position;
-        bodyDef.position.set(position.toVector2());
-        // centering asset
-        asset.setPosition(position.getX() - asset.getWidth()/2, position.getY() - asset.getHeight()/2);
+        body.setTransform(position.toVector2(), 0);
+        asset.setPosition(
+                position.getX() - asset.getWidth()/2f,
+                position.getY() - asset.getHeight()/2f);
     }
 
     public void movePlayer(Vector2 move, float rotation){
         if (move.x == 0 && move.y == 0){
-            moveStop();
+            clearVelocity();
             return;
         }
         asset.setRotation(rotation);
         body.applyLinearImpulse(move, this.position.toVector2(), true);
-//        body.applyForceToCenter(move, true);
-        this.position = new Position2(body.getPosition());
-        asset.setPosition(body.getPosition().x - asset.getWidth()/2, body.getPosition().y - asset.getHeight()/2);
+        this.position.set(body.getPosition());
+        asset.setPosition(
+                body.getPosition().x - asset.getWidth()/2f,
+                body.getPosition().y - asset.getHeight()/2f);
     }
 
     public void update(){
-        this.position = new Position2(body.getPosition());
-        asset.setPosition(body.getPosition().x - asset.getWidth()/2, body.getPosition().y - asset.getHeight()/2);
+        this.position.set(body.getPosition());
+        asset.setPosition(
+                body.getPosition().x - asset.getWidth()/2f,
+                body.getPosition().y - asset.getHeight()/2f);
     }
 
-    public void moveStop(){
+    public void clearVelocity(){
         body.setLinearVelocity(0f,0f);
     }
 
@@ -69,18 +72,6 @@ public class Player extends Entity {
 
     public Position2 getPosition(){
         return position;
-    }
-
-    public Sprite getSprite(){
-        return asset;
-    }
-
-    public int getVelocity() {
-        return velocity;
-    }
-
-    public void setVelocity(int velocity) {
-        this.velocity = velocity;
     }
 
     public void dispose(){
