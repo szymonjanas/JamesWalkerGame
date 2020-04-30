8f11c57c2751223a1f04ac3f58dabb3d0c7b548b
szymon <szymon.janas.95@gmail.com>
2020-04-30 15:06:30 +0200

    Refactor: up to new changes

--- core/src/com/mrwalker/firstgame/Entity/EntityState.java
+++ core/src/com/mrwalker/firstgame/Entity/EntityState.java
@@ -5,12 +5,19 @@ import com.mrwalker.firstgame.auxiliary.Position2;
 
 public class EntityState {
 
+    public Object entityData;
+
+    public EntityState(Object entityData) {
+        this.entityData = entityData;
+    }
+
     public Position2 position;
     public Movements movement;
     public Directions direction;
     public int rotation;
-    public int velocity = 2000000;
-    public int speed = 1;
+    public int velocity = 5000000;
+    public float speed = 1f; // 0-100%
+    public float frameDuration = 1f/9f;
 
     public Vector2 multiplyWithVelocity(Vector2 force){
         return new Vector2(
