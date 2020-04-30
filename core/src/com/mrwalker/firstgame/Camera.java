8625aa348084285719c988a6e87bc39c80d738dd
szymon <szymon.janas.95@gmail.com>
2020-04-30 02:15:16 +0200

    Refactor: change Singleton with static

--- core/src/com/mrwalker/firstgame/Camera.java
+++ core/src/com/mrwalker/firstgame/Camera.java
@@ -6,53 +6,38 @@ import com.mrwalker.firstgame.auxiliary.Position2;
 import com.mrwalker.firstgame.auxiliary.Size2;
 
 public class Camera {
-    private static String TAG = Camera.class.getSimpleName();
+    private static final String TAG = Camera.class.getSimpleName();
 
-    private static Camera cameraInstance = null;
-    private OrthographicCamera camera;
+    private static final OrthographicCamera camera = new OrthographicCamera();;
 
     private static class VIEWPORT{
         static Size2 viewPortSize;
         static Size2 halfViewPortSize;
     }
 
-    private Camera(Position2 position) {
-        camera = new OrthographicCamera();
-        camera.position.x = position.getX();
-        camera.position.y = position.getY();
+    public static void init() {
+        camera.position.x = 0f;
+        camera.position.y = 0f;
         setViewPort(camera.viewportWidth, camera.viewportHeight);
     }
 
-    public static void initInstance(){
-        if (cameraInstance == null){
-            cameraInstance = new Camera(new Position2(0f, 0f)); //default camera coordinates
-        }
-    }
-
-    public static Camera getInstance() {
-        if (cameraInstance == null){
-            Gdx.app.error(TAG, "Camera has no instance!");
-        }
-        return cameraInstance;
-    }
-
-    public OrthographicCamera getCamera() {
+    public static OrthographicCamera getCamera() {
         return camera;
     }
 
-    public void setPosition(Position2 position) {
+    public static void setPosition(Position2 position) {
         camera.position.x = position.getX();
         camera.position.y = position.getY();
         camera.update();
     }
 
-    public Position2 getPosition(){
+    public static Position2 getPosition(){
         return new Position2(
                 camera.position.x ,
                 camera.position.y);
     }
 
-    public void setViewPort(float width, float height){
+    public static void setViewPort(float width, float height){
         VIEWPORT.viewPortSize = new Size2(width, height);
         VIEWPORT.halfViewPortSize = new Size2(width/2, height/2);
         camera.setToOrtho(false, VIEWPORT.viewPortSize.getWidth(),
@@ -60,7 +45,7 @@ public class Camera {
         camera.update();
     }
 
-    public void update(){
+    public static void update(){
         camera.update();
     }
-} //singleton
+}
