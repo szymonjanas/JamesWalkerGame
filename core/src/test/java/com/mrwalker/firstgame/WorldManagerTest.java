df874cd04bbe5b90fac75e632cb37e3547e06a3c
szymon <szymon.janas.95@gmail.com>
2020-04-30 02:26:55 +0200

    Add: Skeleton for JUnit 5

---
+++ core/src/test/java/com/mrwalker/firstgame/WorldManagerTest.java
@@ -0,0 +1,24 @@
+package com.mrwalker.firstgame;
+
+import org.junit.jupiter.api.*;
+
+
+class WorldManagerTest {
+
+    @Test
+    void init() {
+
+    }
+
+    @Test
+    void testInit() {
+    }
+
+    @Test
+    void saveCurrentWorld() {
+    }
+
+    @Test
+    void getWorld() {
+    }
+}
