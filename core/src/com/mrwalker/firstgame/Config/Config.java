0da2b5703ee44354582dba867520ae302a1f8955
szymon <szymon.janas.95@gmail.com>
2020-05-01 02:01:49 +0200

    Add: load and save Config .json files

--- core/src/com/mrwalker/firstgame/Config/Config.java
+++ core/src/com/mrwalker/firstgame/Config/Config.java
@@ -1,6 +1,24 @@
 package com.mrwalker.firstgame.Config;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Json;
+
 public class Config {
-    public static final int tileWidth = 64;
-    public static final int tileHeight = 32;
+    private static final String TAG = Config.class.getSimpleName();
+
+    private static Json json = new Json();
+
+    public static void save(String path, Object object){
+        String config = json.toJson(object);
+        FileHandle file = Gdx.files.local(path);
+        file.writeString(config, false);
+    }
+
+    public static <T> T load(Class<T> type, String path){
+        FileHandle file = Gdx.files.local(path);
+        String config = file.readString();
+        return json.fromJson(type, config);
+    }
+
 }
---
+++ core/src/com/mrwalker/firstgame/Config/Settings.java
@@ -0,0 +1,6 @@
+package com.mrwalker.firstgame.Config;
+
+public class Settings {
+    public static final int tileWidth = 64;
+    public static final int tileHeight = 32;
+}
