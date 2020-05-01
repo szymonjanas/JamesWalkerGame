5b653f8023b38b7e4bff8a840d9270d61900b786
szymon <szymon.janas.95@gmail.com>
2020-05-01 19:15:53 +0200

    Add: HitAndDie system

--- core/src/com/mrwalker/firstgame/Contact/Contact.java
+++ core/src/com/mrwalker/firstgame/Contact/Contact.java
@@ -21,17 +21,14 @@ public class Contact implements ContactListener {
     public void beginContact(com.badlogic.gdx.physics.box2d.Contact contact) {
         Body bodyA = contact.getFixtureA().getBody();
         Body bodyB = contact.getFixtureB().getBody();
-        System.out.println(bodyA.getUserData().getClass().getSimpleName());
-        System.out.println(bodyB.getUserData().getClass().getSimpleName());
 
         EntityIdentification entityIdentificationA = (EntityIdentification) bodyA.getUserData();
-        System.out.println(entityIdentificationA.toString());
-
         EntityIdentification entityIdentificationB = (EntityIdentification) bodyB.getUserData();
-        System.out.println(entityIdentificationB.toString());
 
-        System.out.println(player.checkIdentification(entityIdentificationA));
-        System.out.println(player.checkIdentification(entityIdentificationB));
+        if (entityIdentificationA.getType() == player.getEntityIdentification().getType() ||
+            entityIdentificationB.getType() == player.getEntityIdentification().getType()){
+            player.kill();
+        }
     }
 
     @Override
