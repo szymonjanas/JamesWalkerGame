79ef5aca736a2381850d68589a6bd9c51323d289
szymon <szymon.janas.95@gmail.com>
2020-05-08 14:01:20 +0200

    Add: Frame support class

---
+++ core/src/com/mrwalker/firstgame/Entity/Animations/Frame.java
@@ -0,0 +1,19 @@
+package com.mrwalker.firstgame.Entity.Animations;
+
+public class Frame {
+
+    public final int from;
+    public final int to;
+
+    public Frame(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
+    public int getFrom() {
+        return from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+}
