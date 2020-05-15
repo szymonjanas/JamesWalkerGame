a1cd1d9aac4e15e43c518bdfc227e512673f161a
szymon <szymon.janas.95@gmail.com>
2020-05-15 23:37:04 +0200

    Add: animation config from file

--- core/src/com/mrwalker/firstgame/Entity/Models/Configs/EntityAnimationConfig.java
+++ core/src/com/mrwalker/firstgame/Entity/Models/Configs/EntityAnimationConfig.java
@@ -1,11 +1,14 @@
 package com.mrwalker.firstgame.Entity.Models.Configs;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Json;
 import com.mrwalker.firstgame.Entity.Models.Behaviour;
 import com.mrwalker.firstgame.Entity.Models.Directions;
 
 import java.util.ArrayList;
 
-public class EntityAnimationConfig implements EntityConfig {
+public class EntityAnimationConfig {
     public String type;
     public int correctionFrameX;
     public int correctionFrameY;
@@ -17,4 +20,10 @@ public class EntityAnimationConfig implements EntityConfig {
     public ArrayList<Integer> behaviourFramePosition;
     public ArrayList<Directions> directions;
     public ArrayList<String> animationsPaths;
+
+    public static EntityAnimationConfig getFromFile(String path){
+        FileHandle file = Gdx.files.local(path);
+        Json json = new Json();
+        return json.fromJson(EntityAnimationConfig.class, file.readString());
+    }
 }
