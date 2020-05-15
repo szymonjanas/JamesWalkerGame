858496bfcb6677d9e777750398eaac16a6a09866
szymon <szymon.janas.95@gmail.com>
2020-05-15 23:54:16 +0200

    Add: example velocity

--- core/src/com/mrwalker/firstgame/Config/Settings.java
+++ core/src/com/mrwalker/firstgame/Config/Settings.java
@@ -3,4 +3,5 @@ package com.mrwalker.firstgame.Config;
 public class Settings {
     public static final int tileWidth = 64;
     public static final int tileHeight = 32;
+    public static final int velocity = 2000;
 }
